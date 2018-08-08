870c00c900f9d1db7617aafdd21a8e034854ebfd
Devesh Anand <ananddevesh29.devp@outlook.com>
2018-08-08 12:50:25 -0700

    indent 5

--- lab1/assignment/5.sh
+++ lab1/assignment/5.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-while [1];
+while [ 1 -eq 1 ];
 do
-read command
-case $command in
-"cd")
-read loc
-cd $loc
-;;
-"dir")ls
-;;
-"type")
-read file_name
-cat $file_name
-;;
-"del")
-read file_name
-rm $file_name
-;;
-"ren")
-read old
-read new
-mv $old $new
-;;
-"copy")
-read src
-read dest
-cp $src $dest
-;;
-*)
-echo "$command is not recognised as an external or internal command."
-;;
-esac
+    read command
+    case $command in
+        "cd")
+            read loc
+            cd $loc
+        ;;
+        "dir")ls
+        ;;
+        "type")
+            read file_name
+            cat $file_name
+        ;;
+        "del")
+            read file_name
+            rm $file_name
+        ;;
+        "ren")
+            read old
+            read new
+            mv $old $new
+        ;;
+        "copy")
+            read src
+            read dest
+            cp $src $dest
+        ;;
+        *)
+            echo "$command is not recognised as an external or internal command."
+        ;;
+    esac
 done
